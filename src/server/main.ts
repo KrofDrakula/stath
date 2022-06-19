37d2706dddd323788ac49b5d947b5582fd6ce41a
Klemen Slavič <krof.drakula@gmail.com>
2022-06-19 23:01:33 +0200

    Example router

--- src/server/main.ts
+++ src/server/main.ts
@@ -1,11 +1,18 @@
 import Koa from "koa";
+import Router from "@koa/router";
 
 const port = Number(process.env.PORT || "5000");
 const app = new Koa();
 
-app.use(async (ctx) => {
-  ctx.body = "Hello world!";
+const router = new Router();
+router.get("/listings", async (ctx) => {
+  ctx.body = `Root listing`;
 });
+router.get("/listing/:id", async (ctx) => {
+  ctx.body = `Listing ${ctx.params.id}`;
+});
+
+app.use(router.routes());
 
 app.listen(port);
 
